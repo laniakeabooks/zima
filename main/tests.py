db7e4fa99f7db1e5ab39630012f3e914ecdfa89b
Theodore Keloglou <zf@sirodoht.com>
2024-01-13 03:46:45 +0000

    add tests for index and submit

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from main import models
+
+
+class IndexTest(TestCase):
+    def test_get(self):
+        response = self.client.get(reverse("index"))
+        self.assertEqual(response.status_code, 200)
+
+
+class SubmitTest(TestCase):
+    def test_entry(self):
+        data = {
+            "contact": "email@example.com",
+            "resource": "https://example.com",
+        }
+        response = self.client.post(reverse("index"), data)
+        self.assertEqual(response.status_code, 302)
+        self.assertIn(reverse("submit_success"), response.url)
+        self.assertTrue(models.Entry.objects.filter(contact=data["contact"]).exists())
+        self.assertTrue(models.Entry.objects.filter(resource=data["resource"]).exists())
+        self.assertEqual(models.Entry.objects.all().count(), 1)
