9d452dc22287e06e4c3d08f8bc2dede900a3550d
Theodore Keloglou <zf@sirodoht.com>
2024-01-17 00:16:38 +0000

    reset migrations

--- main/models.py
+++ main/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -13,12 +15,15 @@ class User(AbstractUser):
 
 
 class Entry(models.Model):
+    verify_key = models.UUIDField(default=uuid.uuid4, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    terms = models.BooleanField(default=False)
     email = models.EmailField()
     contact = models.CharField(max_length=300)
     resource = models.URLField()
     has_received = models.ManyToManyField("Entry", through="Matching")
     is_verified = models.BooleanField(default=False)
+    is_approved = models.BooleanField(default=False)
 
     def __str__(self):
         ret = f"[{self.id}]"
@@ -50,3 +55,4 @@ class Matching(models.Model):
 
 class Matchlog(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
+    receiver = models.ForeignKey(Entry, on_delete=models.CASCADE)
