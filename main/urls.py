b5cd601de89429ec99be8ce1b9179f17f39af910
Theodore Keloglou <zf@sirodoht.com>
2024-01-17 00:18:32 +0000

    add verify success url

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,7 @@ from main import views
 urlpatterns = [
     path("", views.CreateEntry.as_view(), name="index"),
     path("submit-success/", views.submit_success, name="submit_success"),
+    path("verify-success/", views.verify_success, name="verify_success"),
     path("about/", views.about, name="about"),
     path("privacy/", views.privacy, name="privacy"),
 ]
