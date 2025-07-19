85b957b11e7aca77bf3ba4c331fa019781bca840
Theodore Keloglou <zf@sirodoht.com>
2025-07-19 17:46:46 +0100

    rename project to zima

--- zima/urls.py
+++ zima/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for z01 project.
+URL configuration for zima project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path
 
